2cf68c71ef557fbeb1c97ec365ef8983d5a601d7
subbu <subbunandyala48@gmail.com>
2025-06-24 20:06:21 +0530

    terrafom

--- variables.tf
+++ variables.tf
@@ -1,20 +1,18 @@
 variable "ami_id" {
-    type = string 
     default = "ami-09c813fb71547fc4f"
-    description = "AMI ID of ec2 instance"
 }
 
 variable "instance_type" {
-    type = string 
-    default = "t3.micro"
-    description = "instance size"
+    default = "t3.small"
+    description = "inastance size"
 
     validation {
-        condition = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
-        error_message = "valid values for instance_type are: t3.micro, t3.small, t3.medium"
+        condition = contains(["t3.micro","t3.small", "t3.medium"], var.instance_type)
+        error_message = "valid values for instance_type are: t3.micro,t3.small,t3.mediun"
     }
-
 }
+    
+
 
 # mandatory to provide 
 variable "sg_ids" {
