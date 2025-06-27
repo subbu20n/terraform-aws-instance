f3a09d6f3872225f81fdb28aad76abff76fe3f63
subbu <subbunandyala48@gmail.com>
2025-06-27 19:05:42 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -1,24 +1,25 @@
 variable "ami_id" {
-    default = "ami-09c813fb71547fc4f"
+    type = string 
+    ami_id = ""
+    description = "AMI ID OF EC2 Instance"
 }
 
 variable "instance_type" {
-    default = "t3.small"
-    description = "inastance size"
+    type  = string 
+    default = "t3.micro"
+    description = "instance size"
 
     validation {
-        condition = contains(["t3.micro","t3.small", "t3.medium"], var.instance_type)
-        error_message = "valid values for instance_type are: t3.micro,t3.small,t3.mediun"
+        condition = contains (["t3.micro", "t3.small", "t3.medium"], var.instance_type)
+        error_message = "valid values for instance_type are: t3.micro,t3small,t3.medium"
     }
 }
-    
 
-
-# mandatory to provide 
+#mandatory to provide 
 variable "sg_ids" {
-    type = list 
+    type  = list 
 }
 
 variable "tags" {
-    type = map 
+    type = map
 }
