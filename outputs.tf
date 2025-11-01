2ff9d0f05782c15b554a2f4218056799b9baee1a
subbu <subbunandyala48@gmail.com>
2025-11-01 16:02:38 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,11 @@
 output "public_ip" {
-      value  = aws_instance.this.public_ip 
+    value = aws_instance.this.public_ip 
 }
+
 output "private_ip" {
     value = aws_instance.this.private_ip 
 }
 
-output "instance_id" {
+output "instnace_id" {
     value = aws_instance.this.id 
 }
